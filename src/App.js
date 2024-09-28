f250152b030d511c233676333ff4519ee0f1e7c0
carlosdos818@yahoo.com <carlosdos818@yahoo.com>
2024-09-28 16:54:41 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Burger Stacker</h1>
+    </div>
+  );
+}
+
+export default App;
